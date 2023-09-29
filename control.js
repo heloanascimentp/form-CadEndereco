cf9cb072885d1edb67dc7221cf3da203dd21d082
Heloá <heloa.dandrea@alunosescolassociais.org.br>
2023-09-29 11:08:30 -0300

    fiz a função de limpar o formulario

--- control.js
+++ control.js
@@ -1,1 +1,11 @@
 'USE strict'; //modo restrito
+//consumindo API de CEP, do ViaCep
+// httsp://viacep.com.br/
+
+//função para limpar o formulario 
+const limparFormulario = () => {
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
